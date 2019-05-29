6bb5faa17e8c2ab39ac0f66f29eb7c6ff245ce21
boriskondev <boriskondev@gmail.com>
2019-05-29 18:45:45 +0300

    Working fine with exports and winners per day.

--- script_xlsx_to_csv_all_save.py
+++ script_xlsx_to_csv_all_save.py
@@ -1,9 +1,10 @@
 from pathlib import Path
-import pandas as pd
 from datetime import datetime
+import pandas as pd
 import re
 
 time_start = datetime.now()
+# print(time_start)
 
 pattern = r"\d{2}\.\d{2}\.\d{4}"
 
@@ -38,33 +39,29 @@ for value_to_skip in values_to_skip:
     indices_to_remove = all_data[(all_data["Transaction"] == value_to_skip)].index
     all_data.drop(indices_to_remove, inplace=True)
 
-winners_to_draw = int(input("\nWinners to draw: "))
-reserves_to_draw = int(input("Reserves to draw: "))
-entries_to_sample = winners_to_draw + reserves_to_draw
-sample_data_frame = all_data.sample(n=entries_to_sample)
-
-winners = sample_data_frame.iloc[:winners_to_draw]
-reserves = sample_data_frame.iloc[winners_to_draw:]
-print(f"Winner/s: \n {winners}")
-print(f"Reserve/s: \n {reserves}")
-
 file_with_all_data = f"C:/Users/pmg23_b.kondev/Desktop/Python/first_python_script/Test folder/All_week_{week_to_process}.csv"
 all_data.to_csv(file_with_all_data, index=False)
 
+winners_to_draw = int(input("\nWinners per day: "))
+reserves_to_draw = int(input("Reserves per day: "))
+entries_to_sample = winners_to_draw + reserves_to_draw
+
 week_dates_list = all_data["Date"].unique()
+
 for date in week_dates_list:
     date_data_frame = all_data[all_data.Date == date]
-    date_file_to_save = f"C:/Users/pmg23_b.kondev/Desktop/Python/first_python_script/Test folder/{date}.csv"
+    date_file_to_save = f"C:/Users/pmg23_b.kondev/Desktop/Python/first_python_script/Test folder/{date}_all.csv"
     date_data_frame.to_csv(date_file_to_save, index=False, header=None)
+    sample_data_frame = date_data_frame.sample(n=entries_to_sample)
+    # sample_data_frame.iloc[0] = ["Winners", None, None]
+    # sample_data_frame.iloc[reserves_to_draw] = ["Reserves", None, None]
+    winners_file_to_save = f"C:/Users/pmg23_b.kondev/Desktop/Python/first_python_script/Test folder/{date}_winners.csv"
+    sample_data_frame.to_csv(winners_file_to_save, index=False, header=None)
     print(f"{date}", end=" ")
-
-sample_data_frame.iloc[0] = ["Winners", None, None]
-sample_data_frame.iloc[reserves_to_draw] = ["Reserves", None, None]
-file_with_all_drawn = f"C:/Users/pmg23_b.kondev/Desktop/Python/first_python_script/Test folder/Winners_week_{week_to_process}.csv"
-sample_data_frame.to_csv(file_with_all_drawn, index=False, header=None)
 
 print("\nAll files are ready!")
 
-time_elapsed = datetime.now()
-time_took = time_elapsed - time_start
+time_end = datetime.now()
+time_took = time_end - time_start
+# print(time_end)
 print(f"The execution of this script took {time_took.seconds} seconds.")
