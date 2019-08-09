9bfac9ee2243fd3a71549ebb373c3baefd994a4f
boriskondev <boriskondev@gmail.com>
2019-08-09 16:27:31 +0300

    Emails throught gmail and file.

--- emails.py
+++ emails.py
@@ -1,15 +1,15 @@
 import smtplib
 
-sender = "boriskondev@gmail.com"
-password = "Painkiller27081986!"
+sender = "pythonemailtests@gmail.com"
+password = "Pythonemailtests!"
 recipient = "b.kondev@publicis-dialog.bg"
 subject = "Test email"
 
 with open("email.txt", "r") as f:
-    email_text = f.read()
+    text = f.read()
 
 smtp_server = smtplib.SMTP_SSL("smtp.gmail.com", 465)
 smtp_server.login(sender, password)
-message = f"Subject: {subject}\n\n{email_text}"
+message = f"Subject: {subject}\n\n{text}"
 smtp_server.sendmail(sender, recipient, message)
 smtp_server.close()
