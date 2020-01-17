b1b8778debbfc2d81a5168042611f3679a328c1d
boriskondev <boriskondev@gmail.com>
2020-01-17 12:41:26 +0200

    17.01.2020

--- read_fifth.py
+++ read_fifth.py
@@ -7,12 +7,15 @@ all_emails = pd.read_excel("test.xlsx", sheet_name=0, skiprows=None)
 
 unique_emails = pd.read_excel("test.xlsx", sheet_name=0, skiprows=None)
 
-unique_emails.drop_duplicates(["Имейл*:"], keep="first", inplace=True)
+unique_emails.drop_duplicates(["Emails"], keep="first", inplace=True)
 
-for email in unique_emails["Имейл*:"]:
-    all_selected = all_emails[all_emails["Имейл*:"] == email]
+for email in unique_emails["Emails"]:
+    all_selected = all_emails[all_emails["Emails"] == email]
     all_selected.index = np.arange(1, len(all_selected) + 1)
     each_fifth = all_selected[(all_selected.index % 5 == 0)]
     all_fifths = all_fifths.append(each_fifth)
 
+all_fifths = all_fifths.sample(n=10)
+
+print("Winners:")
 print(all_fifths)
