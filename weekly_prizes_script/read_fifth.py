7239ac6ccd2ab8b1183ea443f09d079e43bff5be
boriskondev <boriskondev@gmail.com>
2020-01-17 17:06:22 +0200

    17.01.2020

--- weekly_prizes_script/read_fifth.py
+++ weekly_prizes_script/read_fifth.py
@@ -3,9 +3,9 @@ import numpy as np
 
 all_fifths = pd.DataFrame()
 
-all_emails = pd.read_excel("test.xlsx", sheet_name=0, skiprows=None)
+all_emails = pd.read_excel("test4.xlsx", sheet_name=0, skiprows=None)
 
-unique_emails = pd.read_excel("test.xlsx", sheet_name=0, skiprows=None)
+unique_emails = pd.read_excel("test4.xlsx", sheet_name=0, skiprows=None)
 
 unique_emails.drop_duplicates(["Emails"], keep="first", inplace=True)
 
@@ -15,7 +15,7 @@ for email in unique_emails["Emails"]:
     each_fifth = all_selected[(all_selected.index % 5 == 0)]
     all_fifths = all_fifths.append(each_fifth)
 
-all_fifths = all_fifths.sample(n=10)
+#all_fifths = all_fifths.sample(n=10)
 
-print("Winners:")
+#print("Winners:")
 print(all_fifths)
