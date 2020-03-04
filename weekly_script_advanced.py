d09171c93c98c6b987c74e5fb3be5c072421804e
boriskondev <boriskondev@gmail.com>
2020-03-04 19:02:54 +0200

    04.03.2020

--- weekly_script_advanced.py
+++ weekly_script_advanced.py
@@ -1,22 +1,36 @@
+import docx
+from datetime import datetime
 import pandas as pd
+from pathlib import Path
+import os
+import yaml
+
+# Read the weekly file
 
 test_df = pd.read_excel("test.xlsx", sheet_name=0, skiprows=None)
 
+# Split the long date column into two separate columns - date and time of registration
+test_df["Reg_date"], test_df["Reg_time"] = test_df["Submitted date"].str.split(" ").str
 
-# Split the long date column into two separate columns - date and time
+# Create a list with the unique values of the registration dates column
+days_of_week = test_df["Reg_date"].unique()
 
-test_df["Date_clear"], test_df["Time_clear"] = test_df["Submitted date"].str.split(" ").str
+# Create a Pandas Excel writer using XlsxWriter as the engine.
+writer = pd.ExcelWriter("test_multiple.xlsx", engine="xlsxwriter")
 
-test_df.to_excel("test_ready.xlsx", index=False)
+weekly_stats_file = "weekly_stats.txt"
 
+# Filter the entries by day and save all in one file with different sheets
+for day in days_of_week:
+    day_df = test_df[test_df["Reg_date"] == day]
+    with open(weekly_stats_file, "a+") as file:
+        file.write(f"{day}: {day_df.shape[0]}\n")
 
+    #day_df.to_excel(writer, sheet_name=day, index=False)
 
+#writer.save()
 
-
-
-
-
-
+#test_df.to_excel("test_ready.xlsx", index=False)
 
 # import pandas as pd
 # import numpy as np
