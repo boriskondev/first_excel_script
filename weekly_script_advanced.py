c22bf1bfd89db30757555ba0f1c1587786ecd9a8
boriskondev <boriskondev@gmail.com>
2020-03-04 13:12:38 +0200

    04.03.2020

--- weekly_script_advanced.py
+++ weekly_script_advanced.py
@@ -1,21 +1,41 @@
 import pandas as pd
-import numpy as np
 
-all_fifths = pd.DataFrame()
+test_df = pd.read_excel("test.xlsx", sheet_name=0, skiprows=None)
 
-all_emails = pd.read_excel("test4.xlsx", sheet_name=0, skiprows=None)
 
-unique_emails = pd.read_excel("test4.xlsx", sheet_name=0, skiprows=None)
+# Split the long date column into two separate columns - date and time
 
-unique_emails.drop_duplicates(["Emails"], keep="first", inplace=True)
+test_df["Date_clear"], test_df["Time_clear"] = test_df["Submitted date"].str.split(" ").str
 
-for email in unique_emails["Emails"]:
-    all_selected = all_emails[all_emails["Emails"] == email]
-    all_selected.index = np.arange(1, len(all_selected) + 1)
-    each_fifth = all_selected[(all_selected.index % 5 == 0)]
-    all_fifths = all_fifths.append(each_fifth)
+test_df.to_excel("test_ready.xlsx", index=False)
 
-#all_fifths = all_fifths.sample(n=10)
 
-#print("Winners:")
-print(all_fifths)
+
+
+
+
+
+
+
+
+# import pandas as pd
+# import numpy as np
+#
+# all_fifths = pd.DataFrame()
+#
+# all_emails = pd.read_excel("test4.xlsx", sheet_name=0, skiprows=None)
+#
+# unique_emails = pd.read_excel("test4.xlsx", sheet_name=0, skiprows=None)
+#
+# unique_emails.drop_duplicates(["Emails"], keep="first", inplace=True)
+#
+# for email in unique_emails["Emails"]:
+#     all_selected = all_emails[all_emails["Emails"] == email]
+#     all_selected.index = np.arange(1, len(all_selected) + 1)
+#     each_fifth = all_selected[(all_selected.index % 5 == 0)]
+#     all_fifths = all_fifths.append(each_fifth)
+#
+# #all_fifths = all_fifths.sample(n=10)
+#
+# #print("Winners:")
+# print(all_fifths)
