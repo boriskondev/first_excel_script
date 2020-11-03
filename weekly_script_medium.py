47b547ab1c98e6373234983c7c5fe76b64579a83
pmg23_b.kondev <boriskondev@gmail.com>
2020-11-03 18:09:27 +0200

    03.11.2020

--- weekly_script_medium.py
+++ weekly_script_medium.py
@@ -15,6 +15,27 @@ output_path = Path(project_config.paragraphs[1].text.split("=")[1].replace("\"",
 banks_names = yaml.safe_load(project_config.paragraphs[2].text.split("=")[1].replace("\"", ""))  # dict
 codes_to_remove = project_config.paragraphs[3].text.split("=")[1].strip().replace("\"", "").split(", ")  # list
 
-# Join config and file codes
-# Fix commas
-# And so on
+statistics = []
+
+week_to_process = 1
+daily_winners = 5
+daily_reserves = 5
+weekly_winners = 1
+weekly_reserves = 5
+
+whole_week = ""
+
+os.chdir(output_path)
+
+for element in source_path.glob("*"):
+    if os.path.isdir(element):
+        if element.name != "_Results" and int(element.name.split(".")[0]) == week_to_process:
+            for file in element.iterdir():
+                if file.suffix == ".xlsx":
+
+                    all_df = pd.read_excel(file, sheet_name=0, skiprows=None)
+                    all_df.columns = all_df.columns.str.strip()
+                    all_df["Банка*:"] = all_df["Банка*:"].str.strip()
+                    all_df["Reg_date"], all_df["Reg_time"] = all_df["Submitted date"].str.split(" ").str
+
+                    all_df.to_excel("test.xlsx", index=False)
