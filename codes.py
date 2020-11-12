83dd5f7edbc9ab6fb79397e2d835eb4e9769f02a
pmg23_b.kondev <boriskondev@gmail.com>
2020-11-12 17:56:23 +0200

    12.11.2020

---
+++ codes.py
@@ -0,0 +1,23 @@
+new_codes = []
+
+code = input().strip()
+
+while code != "END":
+    new_codes.append(code)
+    code = input().strip()
+
+print(new_codes)
+
+# АВТ. КОД :
+# Auth. Code:
+# Auth. Code:
+# AC,:
+# AUTH. CODE :
+# АВТ.КОД/АС :
+# АВТ.КОД/АС :
+# Авт.код /AC
+# Auth. Code:
+# Авт. Код :
+# Авт. Код :
+# AUTH. CODE :
+# END
