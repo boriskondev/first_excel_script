022654de6c27762ed696c7bc365296880d1fff37
pmg23_b.kondev <boriskondev@gmail.com>
2021-07-17 17:20:18 +0300

    17.07.2021

--- oli_script.py
+++ oli_script.py
@@ -26,16 +26,11 @@ def process_code(data, current_code):
     return data
 
 
-# time_start = datetime.now()
-
-project_config = docx.Document("config_new.docx")
-
-SOURCE_PATH = Path(project_config.paragraphs[0].text.split("=")[1].replace("\"", ""))
-OUTPUT_PATH = Path(project_config.paragraphs[1].text.split("=")[1].replace("\"", ""))
-BANKS_EN_NAME = yaml.safe_load(project_config.paragraphs[2].text.split("=")[1].replace("\"", ""))
-CODES_TO_REMOVE = project_config.paragraphs[3].text.split("=")[1].strip().replace("\"", "").split(", ")
-
-statistics = []
+PROJECT_CONFIG = docx.Document("config_new.docx")
+SOURCE_PATH = Path(PROJECT_CONFIG.paragraphs[0].text.split("=")[1].replace("\"", ""))
+OUTPUT_PATH = Path(PROJECT_CONFIG.paragraphs[1].text.split("=")[1].replace("\"", ""))
+BANKS_DICT = yaml.safe_load(PROJECT_CONFIG.paragraphs[2].text.split("=")[1].replace("\"", ""))
+CODES_TO_REMOVE = PROJECT_CONFIG.paragraphs[3].text.split("=")[1].strip().replace("\"", "").split(", ")
 
 WEEK_TO_PROCESS = 1
 WEEKLY_EACH_NTH = 3
@@ -55,6 +50,8 @@ DAILY_WINNERS = DAILY_P1_COUNT + DAILY_P2_COUNT + DAILY_P3_COUNT
 DAILY_RESERVES = DAILY_WINNERS
 
 whole_week = ""
+statistics = []
+time_start = datetime.now()
 
 os.chdir(OUTPUT_PATH)
 
@@ -100,56 +97,56 @@ for element in SOURCE_PATH.glob("*"):
                     weekly_all = f"Week_{whole_week}_all_registrations.xlsx"
                     weekly_duplicates = f"Week_{whole_week}_duplicates.xlsx"
                     weekly_without_duplicates = f"Week_{whole_week}_without_duplicates.xlsx"
-                    weekly_thirds = f"Week_{whole_week}_thirds.xlsx"
+                    weekly_nths = f"Week_{whole_week}_thirds.xlsx"
 
-                    current_files = [weekly_all, weekly_duplicates, weekly_without_duplicates, weekly_thirds]
+                    current_files = [weekly_all, weekly_duplicates, weekly_without_duplicates, weekly_nths]
 
                     for current_file in current_files:
                         if os.path.exists(current_file):
                             os.remove(current_file)
 
                     initial_df.to_excel(weekly_all, index=False)
-                    # statistics = append_and_print_statistics(f"Total registrations: {initial_df.shape[0]}", statistics)
+                    statistics = append_and_print_statistics(f"Total registrations: {initial_df.shape[0]}", statistics)
 
                     duplicates_df.to_excel(weekly_duplicates, index=False)
-                    # statistics = append_and_print_statistics(f"Duplicates: {duplicates_df.shape[0]}", statistics)
+                    statistics = append_and_print_statistics(f"Duplicates: {duplicates_df.shape[0]}", statistics)
 
                     all_df.to_excel(weekly_without_duplicates, index=False)
-                    # statistics = append_and_print_statistics(f"Without duplicates: {all_df.shape[0]}", statistics)
+                    statistics = append_and_print_statistics(f"Without duplicates: {all_df.shape[0]}", statistics)
 
                     # Weekly prize logic
-                    thirds_df = pd.DataFrame()
+                    nths_df = pd.DataFrame()
                     emails_list = all_df["Email address (Personal)"].unique()
-                    # statistics = append_and_print(f"Unique emails: {len(emails_list)}", statistics)
+                    statistics = append_and_print_statistics(f"Unique emails: {len(emails_list)}", statistics)
 
                     for email in emails_list:
                         all_selected = all_df[all_df["Email address (Personal)"] == email]
                         all_selected.index = np.arange(1, len(all_selected) + 1)
-                        each_third = all_selected[(all_selected.index % WEEKLY_EACH_NTH == 0)]
-                        thirds_df = thirds_df.append(each_third)
+                        each_nth = all_selected[(all_selected.index % WEEKLY_EACH_NTH == 0)]
+                        nths_df = nths_df.append(each_nth)
 
-                    thirds_df.to_excel(weekly_thirds, index=False)
+                    nths_df.to_excel(weekly_nths, index=False)
 
                     os.chdir("../../")
                     os.chdir(weekly_winners_folder)
 
                     writer = pd.ExcelWriter(f"Week_{whole_week}_weekly_winners_and_reserves.xlsx", engine="xlsxwriter")
 
-                    weekly_drawn_df = thirds_df.sample(n=WEEKLY_WINNERS + WEEKLY_RESERVES)
+                    weekly_drawn_df = nths_df.sample(n=WEEKLY_WINNERS + WEEKLY_RESERVES)
                     weekly_drawn_df = weekly_drawn_df.reset_index(drop=True)
-                    weekly_winners_df = weekly_drawn_df.loc[0:(WEEKLY_WINNERS - 1)]
-                    weekly_winners_df.insert(0, "Status", "Седмична награда")
-                    weekly_reserves_df = weekly_drawn_df.loc[WEEKLY_WINNERS:(WEEKLY_WINNERS + WEEKLY_RESERVES - 1)]
-                    weekly_reserves_df.insert(0, "Status", "Седмична резерва")
+                    all_weekly_winners_df = weekly_drawn_df.loc[0:(WEEKLY_WINNERS - 1)]
+                    all_weekly_winners_df.insert(0, "Status", "Седмична награда")
+                    all_weekly_reserves_df = weekly_drawn_df.loc[WEEKLY_WINNERS:(WEEKLY_WINNERS + WEEKLY_RESERVES - 1)]
+                    all_weekly_reserves_df.insert(0, "Status", "Седмична резерва")
 
-                    weekly_winners_df.to_excel(writer, sheet_name="Weekly_winners", index=False)
-                    weekly_reserves_df.to_excel(writer, sheet_name="Weekly_reserves", index=False)
+                    all_weekly_winners_df.to_excel(writer, sheet_name="Weekly_winners", index=False)
+                    all_weekly_reserves_df.to_excel(writer, sheet_name="Weekly_reserves", index=False)
 
                     writer.save()
 
-                    # statistics = append_and_print(f"Weekly prize entries: {fifths_df.shape[0]}", statistics)
-                    # unique_emails_count = len(thirds_df['Email address (Personal)'].unique())
-                    # statistics = append_and_print(f"Weekly prize unique emails: {unique_emails_count}", statistics)
+                    statistics = append_and_print_statistics(f"Weekly prize entries: {nths_df.shape[0]}", statistics)
+                    unique_emails_count = len(nths_df["Email address (Personal)"].unique())
+                    statistics = append_and_print_statistics(f"Weekly prize unique emails: {unique_emails_count}\n", statistics)
 
                     # Daily prizes logic
                     all_daily_winners, all_daily_reserves = [], []
@@ -160,7 +157,7 @@ for element in SOURCE_PATH.glob("*"):
 
                     for day in days_of_week:
                         daily_df = all_df[all_df["Reg_date"] == day]
-                        # statistics = append_and_print(f"Entries for {day}: {daily_df.shape[0]}", statistics)
+                        statistics = append_and_print_statistics(f"Entries for {day}: {daily_df.shape[0]}", statistics)
                         daily_drawn_df = daily_df.sample(n=DAILY_WINNERS + DAILY_RESERVES)
                         daily_drawn_df = daily_drawn_df.reset_index(drop=True)
                         daily_winners_df = daily_drawn_df.loc[0:(DAILY_WINNERS - 1)]
@@ -178,49 +175,58 @@ for element in SOURCE_PATH.glob("*"):
 
                     writer.save()
 
+                    writer = pd.ExcelWriter(f"Week_{whole_week}_all_winners_and_reserves.xlsx", engine="xlsxwriter")
 
-                    # winning_banks_folder = "For banks"
-                    #
-                    # if not os.path.exists(winning_banks_folder):
-                    #     os.makedirs(winning_banks_folder)
-                    #
-                    # os.chdir(winning_banks_folder)
-                    #
-                    # statistics = append_and_print_statistics("\nWinners by bank:", statistics)
-                    #
-                    # for bank in banks_en_names:
-                    #     bank_info_to_write = ""
-                    #     if bank in weekly_winners_df["Банка*:"].unique():
-                    #         winning_bank_df = weekly_winners_df[weekly_winners_df["Банка*:"] == bank]
-                    #         bank_info_to_write = f"{banks_en_names[bank]}: {winning_bank_df.shape[0]}"
-                    #         bank_name = banks_en_names[bank]
-                    #
-                    #         if winning_bank_df.shape[0] == 1:
-                    #             win_bank_file = f"Week_{whole_week}_winner_{bank_name}.xlsx"
-                    #         else:
-                    #             win_bank_file = f"Week_{whole_week}_winners_{bank_name}.xlsx"
-                    #
-                    #         if os.path.isfile(win_bank_file):
-                    #             os.remove(win_bank_file)
-                    #
-                    #         winning_bank_df.to_excel(win_bank_file, index=False)
-                    #     else:
-                    #         bank_info_to_write = f"{banks_en_names[bank]}: 0"
-                    #
-                    #     statistics = append_and_print_statistics(bank_info_to_write, statistics)
+                    all_winners = all_weekly_winners_df.append(all_daily_winners_df)
+                    all_reserves = all_weekly_reserves_df.append(all_daily_reserves_df)
 
-# os.chdir("../../")
-#
-# weekly_stats_file = f"Week_{whole_week}_statistics.txt"
-#
-# if os.path.exists(weekly_stats_file):
-#     os.remove(weekly_stats_file)
-#
-# with open(weekly_stats_file, "a+") as file:
-#     for row in statistics:
-#         file.write(f"{row}\n")
-#
-# time_end = datetime.now()
-# time_took = time_end - time_start
-#
-# print(f"\nDone! The execution of this script took {time_took.seconds} seconds.")
+                    all_winners.to_excel(writer, sheet_name="All_winners", index=False)
+                    all_reserves.to_excel(writer, sheet_name="All_reserves", index=False)
+
+                    writer.save()
+
+                    winning_banks_folder = "For banks"
+
+                    if not os.path.exists(winning_banks_folder):
+                        os.makedirs(winning_banks_folder)
+
+                    os.chdir(winning_banks_folder)
+
+                    statistics = append_and_print_statistics("\nWinners by bank:", statistics)
+
+                    for bank in BANKS_DICT:
+                        bank_info_to_write = ""
+                        if bank in all_winners["Bank"].unique():
+                            winning_bank_df = all_winners[all_winners["Bank"] == bank]
+                            bank_info_to_write = f"{BANKS_DICT[bank]}: {winning_bank_df.shape[0]}"
+                            bank_name = BANKS_DICT[bank]
+
+                            if winning_bank_df.shape[0] == 1:
+                                win_bank_file = f"Week_{whole_week}_winner_{bank_name}.xlsx"
+                            else:
+                                win_bank_file = f"Week_{whole_week}_winners_{bank_name}.xlsx"
+
+                            if os.path.isfile(win_bank_file):
+                                os.remove(win_bank_file)
+
+                            winning_bank_df.to_excel(win_bank_file, index=False)
+                        else:
+                            bank_info_to_write = f"{BANKS_DICT[bank]}: 0"
+
+                        statistics = append_and_print_statistics(bank_info_to_write, statistics)
+
+os.chdir("../../")
+
+weekly_stats_file = f"Week_{whole_week}_statistics.txt"
+
+if os.path.exists(weekly_stats_file):
+    os.remove(weekly_stats_file)
+
+with open(weekly_stats_file, "a+") as file:
+    for row in statistics:
+        file.write(f"{row}\n")
+
+time_end = datetime.now()
+time_took = time_end - time_start
+
+print(f"\nDone! The execution of this script took {time_took.seconds} seconds.")
